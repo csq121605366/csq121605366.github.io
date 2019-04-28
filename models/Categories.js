40095493f28d2aa9affa7ab99a358847deb56f9d
caishangqing <121605366@qq.com>
2019-04-28 11:49:02 +0800

    z

--- models/Categories.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by csq on 2017/8/15.
- */
-
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const categoriesSchema = require('../schemas/categories');
-module.exports = mongoose.model('Categories',categoriesSchema);
--- models/User.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by csq on 2017/8/15.
- */
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const userSchema = require('../schemas/users');
-module.exports = mongoose.model('User', userSchema);
-
